bc9a32cab2d5398db4b378aa70770500250eb411
hyunwoome <hyunwoome@icloud.com>
2021-04-21 12:45:24 +0900

    feat: monthly 내적 추가

---
+++ programmers/monthly/음양더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+  let answer = 0;
+  for (let i = 0; i < signs.length; i++) {
+    signs[i] ? (answer += absolutes[i]) : (answer -= absolutes[i]);
+  }
+  return answer;
+}
+
+console.log(solution([1, 2, 3], [false, false, true]));
