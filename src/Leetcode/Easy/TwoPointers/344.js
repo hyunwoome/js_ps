be752376787c3d01f0f31217b3a5cf0cdbd9873e
hyunwoome <hyunwoome@icloud.com>
2021-07-14 16:34:54 +0900

    algo: LeetCode 344

---
+++ src/Leetcode/Easy/TwoPointers/344.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    let tmp = s[i];
+    s[i] = s[j];
+    s[j] = tmp;
+    i++;
+    j--;
+  }
+  return s;
+};
+
+const s = ['h', 'e', 'l', 'l', 'o'];
+console.log(reverseString(s));
