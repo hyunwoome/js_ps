2e94a39ece9c8fb5f32c7e8c4485b5e3020810f7
hyunwoome <hyunwoome@icloud.com>
2021-09-17 13:10:22 +0900

    solve: Goorm ASCII 코드

---
+++ src/Goorm/LV1/ASCII코드.js
@@ -0,0 +1,13 @@
+// Run by Node.js
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (line) {
+  console.log(line.charCodeAt());
+}).on('close', function () {
+  process.exit();
+});
