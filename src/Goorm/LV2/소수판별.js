579d1ade2d1e7f38037d91db7bb38b120117951a
hyunwoome <hyunwoome@icloud.com>
2021-09-20 12:43:28 +0900

    solve: Goorm 소수 판별

---
+++ src/Goorm/LV2/소수판별.js
@@ -0,0 +1,23 @@
+// Run by Node.js
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (line) {
+  const num = +line;
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) count++;
+  }
+  if (count === 2) {
+    console.log('True');
+  } else {
+    console.log('False');
+  }
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
