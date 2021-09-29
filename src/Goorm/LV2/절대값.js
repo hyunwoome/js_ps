6f6d6650ebc05d622c287bd13c54b4114b05e2ee
hyunwoome <hyunwoome@icloud.com>
2021-09-29 13:38:05 +0900

    solve: Goorm 절대값

--- src/Goorm/LV2/절대값.js
+++ src/Goorm/LV2/절대값.js
@@ -7,18 +7,7 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', function (line) {
-  const num = +line;
-  let count = 0;
-  for (let i = 1; i <= num; i++) {
-    if (num % i === 0) count++;
-  }
-  if (count === 2) {
-    console.log('True');
-  } else {
-    console.log('False');
-  }
-
-  rl.close();
+  console.log(Math.abs(+line));
 }).on('close', function () {
   process.exit();
 });
