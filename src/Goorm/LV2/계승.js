ea56cac0e7fad3f3cb31aeea298a48c5fdcfe870
hyunwoome <hyunwoome@icloud.com>
2021-10-17 13:04:27 +0900

    solve: Goorm 계승

---
+++ src/Goorm/LV2/계승.js
@@ -0,0 +1,18 @@
+// Run by Node.js
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on("line", function(line) {
+  const input = +line;
+  const fact = (num) => {
+    if (num === 0) return 1;
+    else return num * fact( num - 1 );
+  };
+  console.log(fact(input));
+}).on("close", function() {
+  process.exit();
+});
