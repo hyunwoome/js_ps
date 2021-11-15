5ffe471f91e9418681bf078a45b6273ce7c51f0d
Lim Hyunwoo <hyunwoome@icloud.com>
2021-11-15 11:35:50 +0900

    solve: LeetCode 704

---
+++ src/Leetcode/Easy/BinarySearch/704.js
@@ -0,0 +1,17 @@
+const search = (nums, target) => {
+  let low = 0;
+  let high = nums.length - 1;
+  while (low < high) {
+    let mid = low + Math.floor((high - low + 1) / 2);
+    if (target < nums[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid;
+    }
+  }
+  return nums[low] == target ? low : -1;
+};
+
+const nums = [-1, 0, 3, 5, 9, 12];
+const target = 9;
+console.log(search(nums, target));
