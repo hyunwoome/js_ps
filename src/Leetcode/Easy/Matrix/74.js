da570c9a97807d0a165e718ac653f20aeb9ff2fd
Lim Hyunwoo <hyunwoome@icloud.com>
2021-11-19 20:32:12 +0900

    solve: LeetCode 74

---
+++ src/Leetcode/Easy/Matrix/74.js
@@ -0,0 +1,16 @@
+const searchMatrix = (matrix, target) => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === target) return true;
+    }
+  }
+  return false;
+};
+
+const matrix = [
+  [1, 3, 5, 7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 60],
+];
+const tartget = 3;
+console.log(searchMatrix(matrix, tartget));
